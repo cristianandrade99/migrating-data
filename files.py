2649da3d388755d98cfa90a00631c618cb6e2301
cristianandrade99 <cristian.andrade@spicewise.co>
2022-02-23 19:12:30 -0500

    working

--- files.py
+++ files.py
@@ -2,36 +2,69 @@ import os
 import sys
 import datetime
 import shutil
+import json
+import time
+from pick import pick
+from tqdm import tqdm
 
+prepare='prepare'
+migrate='migrate'
+exit_process = 'Salir'
 source = sys.argv[1]
+action = sys.argv[2]
 destiny = 'D:/test'
-image_video_extensions=['.jpg','.jpeg','.JPG','.gif','.png','.AVI','.wmv','.MOV','.BMP','.PNG','.mp4','.CR2','.3gp']
+json_destiny='./paths.json'
+progress_destiny='./progress.json'
+image_video_extensions=[
+'.jpg',
+'.png',
+'.jpeg',
+'.mp4',
+'.JPG',
+'.gif',
+'.MOV',
+'.m4a',
+'.3gp',
+'.JPG2468',
+'.AVI',
+'.wmv',
+'.BMP',
+'.NEF',
+'.PNG',
+'.CR2',
+'.avi'
+]
 months = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
 
 def print_array(array):
     for item in array:
         print(item)
 
-def get_all_file_paths ():
+def save_json(data,directory):
+    json_object = json.dumps(data,ensure_ascii=False,indent=2)
+    with open(directory, 'w', encoding='utf8') as outfile:
+        outfile.write(json_object)
+
+def get_date_data(key):
+    year=key[0:4]
+    month=key[5:7]
+    return year,month
+
+def get_all_file_paths():
     paths=[];
     for root, _, files in os.walk(source):
         for file in files:
             path = os.path.join(root,file)
             _,ext = os.path.splitext(path)
-            creation = datetime.datetime.fromtimestamp(os.path.getctime(path))
             modification = datetime.datetime.fromtimestamp(os.path.getmtime(path))
-            #creation_format = creation.strftime('%Y-%m')
-            key = modification.strftime('%Y%m%d')
-            year = modification.strftime('%Y')
-            month = modification.strftime('%m')
-            day = modification.strftime('%d')
-            paths.append([path,ext,key,year,month,day])
+            key = modification.strftime('%Y-%m')
+            paths.append([path,ext,key])
     return paths
 
 def get_all_different_extensions(paths):
     extensions=[]
     for current_path in paths:
-        ext=current_path[1]
+        _,ext,_=current_path
         if ext not in extensions:
             extensions.append(ext)
     return extensions
@@ -39,42 +72,93 @@ def get_all_different_extensions(paths):
 def get_image_video_paths(paths):
     image_video_paths=[]
     for current_path in paths:
-        ext=current_path[1]
+        _,ext,_=current_path
         if ext in image_video_extensions:
             image_video_paths.append(current_path)
     return image_video_paths
 
-def group_by_year_month(paths):
+def get_group_by_date(paths):
     groups={}
     for current_path in paths:
-        key = current_path[2]
-        year = current_path[3]
-        month = current_path[4]
-        day = current_path[5]
+        path,_,key=current_path
         if key not in groups:
-            groups[key]={'year':year,'month':month,'day':day,'paths':[current_path]}
+            groups[key]=[path]
         else:
-            groups[key]['paths'].append(current_path)
+            groups[key].append(path)
     return groups
 
-paths = get_all_file_paths()
-extensions = get_all_different_extensions(paths)
-image_video_paths = get_image_video_paths(paths)
-groups = group_by_year_month(image_video_paths)
+def get_list(groups):
+    list_group=[]
+    for key,value in groups.items():
+        list_group.append({'key':key,'paths':value})
+    list_group.sort(key=lambda group: group.get('key'))
+    return list_group
 
-for key in groups:
-    if('201908' in key):
-        item = groups[key]
-        year = item['year']
-        month = item['month']
-        day = item['day']
-        directory=os.path.join(destiny,year)
-        try:
-            os.makedirs(directory)
-        except:
-            pass
-        print(f'{year}/{month}')
-        for current_path in item['paths']:
-            path = current_path[0]
-            print(path)
-            shutil.copy2(path,directory)
+def register_groups(array):
+    save_json(array,json_destiny)
+    progress=[]
+    for group in array:
+        key = group['key']
+        progress.append({'key':key,'migrated':False})
+    save_json(progress,progress_destiny)
+
+def load_data(source):
+    with open(source,encoding='utf8') as file:
+        data = json.load(file)
+        return data
+
+def find_index(array,value):
+    index=-1
+    for idx,item in enumerate(array):
+        if item['key']==value:
+            index=idx
+            break
+    return index
+
+def save_progess(progress,new_index):
+    progress[new_index]['migrated']=True
+    save_json(progress,progress_destiny)
+
+def create_directory(directory):
+    try:
+        os.makedirs(directory)
+    except:
+        pass
+
+def migrate_data():
+    data = load_data(json_destiny)
+    progress = load_data(progress_destiny)
+    title = 'Escoge hasta qué fecha migrar'
+    continue_app=True
+    while(continue_app):
+        options = [item['key'] for item in progress if item['migrated'] == False]+[exit_process]
+        selected_option,_ = pick(options, title)
+        if(selected_option==exit_process):
+            continue_app=False
+        else:
+            for current_option in options:
+                new_index = find_index(progress,current_option)
+                if(new_index!=-1):
+                    paths = data[new_index]['paths']
+                    year,_=get_date_data(current_option)
+                    directory=os.path.join(destiny,year)
+                    create_directory(directory)
+                    tqdm_paths=tqdm(paths)
+                    tqdm_paths.set_description(current_option)
+                    for path in tqdm_paths:
+                        shutil.copy2(path,directory)
+                    save_progess(progress,new_index)
+                if current_option==selected_option:
+                    break   
+
+if action==prepare:
+    all_file_paths=get_all_file_paths()
+    image_video_paths=get_image_video_paths(all_file_paths)
+    group_by_date=get_group_by_date(image_video_paths)
+    array=get_list(group_by_date)
+    register_groups(array)
+elif action==migrate:
+    migrate_data()
+
+# cd C:\Users\Games\Downloads\migration-data 
+# python files.py C:\Users\Games\Documents\Migracion prepare
--- files2.py
+++ files2.py
@@ -2,44 +2,86 @@ import sys
 import os
 import datetime
 import shutil
+import json
 
+instructions_dir='./create_folders.json'
+work_folder = 'D:/test/'
 months = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
-[_,path,start,end,name]=sys.argv
 
-def print_array(array):
-    for item in array:
-        print(item)
+def load_data(source):
+    with open(source,encoding='utf8') as file:
+        data = json.load(file)
+        return data
 
-def list_dirs():
-    dirs=os.listdir(path)
+def list_dirs(path):
+    dirs = os.listdir(path)
     return dirs
 
-def get_dirs_to_move(dirs):
-    dirs_to=[]
-    month = ''
-    month_label=''
-    if(len(dirs)>0):
-        current_path = os.path.join(path,dirs[0])
-        month=datetime.datetime.fromtimestamp(os.path.getmtime(current_path)).strftime('%m')
-        month_label = months[int(month)-1]
-    for dir_item in dirs:
-        current_path = os.path.join(path,dir_item)
-        if os.path.isfile(current_path):
-            modification = datetime.datetime.fromtimestamp(os.path.getmtime(current_path))
-            day = int(modification.strftime('%d'))
-            if day >= int(start) and day <= int(end):
-                dirs_to.append(current_path)
-    return dirs_to,month,month_label
+def create_directory(directory):
+    try:
+        os.makedirs(directory)
+    except:
+        pass
 
-def move(dirst_to_move,month,month_label):
-    for current_path in dirst_to_move:
-        directory=os.path.join(path,f'{month} - {month_label} - {name}')
-        try:
-            os.makedirs(directory)
-        except:
-            pass
-        shutil.move(current_path,directory)
+def run():
+    instructions=load_data(instructions_dir)
+    year=instructions['year']
+    groups=instructions['groups']
+    current_path = os.path.join(work_folder,year)
+    for group in groups:
+        start=group['start']
+        end=group['end']
+        title=group['title']
+        paths=list_dirs(current_path)
+        if(len(paths)>0):
+            month=datetime.datetime.fromtimestamp(os.path.getmtime(current_path)).strftime('%m')
+            month_label = months[int(month)-1]
+            new_path = os.path.join(current_path,f'{month} - {month_label} - {title}')
+            for path in paths:
+                belongs = start in path or end in path or (path>=start and path <= end)
+                if belongs:
+                    new_file_path=os.path.join(new_path,path)
+                    print(new_file_path)
 
-dirs = list_dirs()
-dirst_to_move,month,month_label = get_dirs_to_move(dirs)
-move(dirst_to_move,month,month_label)
+run()
+
+# months = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
+# [_,path,start,end,name]=sys.argv
+
+# def print_array(array):
+#     for item in array:
+#         print(item)
+
+# def list_dirs():
+#     dirs=os.listdir(path)
+#     return dirs
+
+# def get_dirs_to_move(dirs):
+#     dirs_to=[]
+#     month = ''
+#     month_label=''
+#     if(len(dirs)>0):
+#         current_path = os.path.join(path,dirs[0])
+#         month=datetime.datetime.fromtimestamp(os.path.getmtime(current_path)).strftime('%m')
+#         month_label = months[int(month)-1]
+#     for dir_item in dirs:
+#         current_path = os.path.join(path,dir_item)
+#         if os.path.isfile(current_path):
+#             modification = datetime.datetime.fromtimestamp(os.path.getmtime(current_path))
+#             day = int(modification.strftime('%d'))
+#             if day >= int(start) and day <= int(end):
+#                 dirs_to.append(current_path)
+#     return dirs_to,month,month_label
+
+# def move(dirst_to_move,month,month_label):
+#     for current_path in dirst_to_move:
+#         directory=os.path.join(path,f'{month} - {month_label} - {name}')
+#         try:
+#             os.makedirs(directory)
+#         except:
+#             pass
+#         shutil.move(current_path,directory)
+
+# dirs = list_dirs()
+# dirst_to_move,month,month_label = get_dirs_to_move(dirs)
+# move(dirst_to_move,month,month_label)
